806d8c71d162cbd37e126b1579f2197b37890de5
Sebastian <sebastian.padin@vallegrande.edu.pe>
2025-06-20 13:59:10 -0500

    microservicio de usuario

---
+++ src/test/java/pe/edu/vallegrande/user_service/UserServiceApplicationTests.java
@@ -0,0 +1,15 @@
+package pe.edu.vallegrande.user_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+
+@Import(TestcontainersConfiguration.class)
+@SpringBootTest
+class UserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
