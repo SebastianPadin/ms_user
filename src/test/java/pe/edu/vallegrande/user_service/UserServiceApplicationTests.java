83faf205aeacaff9e938f58fc7cfc14d17d04bfa
Sebastian Padin <126886545+SebastianPadin@users.noreply.github.com>
2025-06-20 20:23:13 +0000

    BUILD SUCCES

--- src/test/java/pe/edu/vallegrande/user_service/UserServiceApplicationTests.java
+++ src/test/java/pe/edu/vallegrande/user_service/UserServiceApplicationTests.java
@@ -2,9 +2,8 @@ package pe.edu.vallegrande.user_service;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Import;
 
-@Import(TestcontainersConfiguration.class)
+
 @SpringBootTest
 class UserServiceApplicationTests {
 
